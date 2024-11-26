e91b24d7db1d9d989e53c40c8d0451204daefa88
Vijay Subramanian <47090672+baranidar@users.noreply.github.com>
2024-11-26 11:00:57 -0800

    Create hash_map.py

---
+++ hash_map.py
@@ -0,0 +1,31 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.arr = [None for i in range(self.MAX)]
+        
+    def get_hash(self, key):
+        hash = 0
+        for i in key:
+            hash += ord(i)
+            return hash % self.MAX
+            
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h] if self.arr[h] is not None else "Not Found"
+        
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+        
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+        
+        
+t = HashTable()
+t["March 16"] = 120
+t["Feb 20"] = 130
+print(t["March 16"])
+print(t["Feb 20"])
+del t["Feb 20"]
+print(t["Feb 20"])
